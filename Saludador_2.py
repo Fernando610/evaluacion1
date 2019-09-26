09a6eba81204dfd6b03edad5c8d65836275988e5
Fernando Baena <55531240+Fernando610@users.noreply.github.com>
2019-09-26 09:36:34 +0200

    Add files via upload

---
+++ Saludador_2.py
@@ -0,0 +1,7 @@
+def saludador_2():
+    print "hola amigo" #Instruccion de salida
+    nombre= raw_input("Como te llamas?: ") #Instruccion de entrada
+    print "Bienvenido a Python "+ nombre
+
+saludador_2()
+
