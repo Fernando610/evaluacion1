ae3260027f3ea16d5588c880fe00536dedfc9458
Fernando Baena <55531240+Fernando610@users.noreply.github.com>
2019-10-15 11:16:50 +0200

    Add files via upload

---
+++ contador.py
@@ -0,0 +1,10 @@
+#EJemplo de for (forense)
+def ejemplo_for():
+    #i es una variable que toma los valores comprendidos en el rango
+    a=input ("en que numero quieres que empiece: ")
+    b=input ("en que numero quieres que termine: ")
+    for i in range(a,b):
+        print "i= ",i
+
+ejemplo_for () 
+    
