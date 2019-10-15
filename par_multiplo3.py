4e2ee7fad47e4fc158bd5fe66e0968c1df2ad00e
Fernando Baena <55531240+Fernando610@users.noreply.github.com>
2019-10-15 11:41:33 +0200

    Add files via upload

---
+++ par_multiplo3.py
@@ -0,0 +1,13 @@
+def par_multiplo3():
+    print "dime un numero"
+    a=input ("A=")
+    if (a%2==0):
+        print "es par"
+    else:
+        print "es impar"
+    if (a%3==0):
+        print "es multiplo de 3"
+    else:
+        print "no es multiplo de 3"
+    
+par_multiplo3()
